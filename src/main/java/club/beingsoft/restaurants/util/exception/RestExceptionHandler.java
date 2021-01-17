33643b8dc823d1f00fe9b95272be5322e343ba19
evgeny-grigorev <evgeny.v.grigorev@yandex.ru>
2021-01-17 22:16:43 +0400

    uncomment exception handler

--- src/main/java/club/beingsoft/restaurants/util/exception/RestExceptionHandler.java
+++ src/main/java/club/beingsoft/restaurants/util/exception/RestExceptionHandler.java
@@ -4,10 +4,11 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@RestControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
